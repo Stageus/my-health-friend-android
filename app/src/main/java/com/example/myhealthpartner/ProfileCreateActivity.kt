f11e80045af73c65b442eba7ec18b3f0b4356f59
hyosoek <tennfin1@gmail.com>
2022-08-02 18:48:48 +0900

    Little issue solve

--- app/src/main/java/com/example/myhealthpartner/ProfileCreateActivity.kt
+++ app/src/main/java/com/example/myhealthpartner/ProfileCreateActivity.kt
@@ -1,12 +1,15 @@
 package com.example.myhealthpartner
 
+import android.app.AlertDialog
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.location.Address
+import android.location.Geocoder
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
-import androidx.fragment.app.FragmentTransaction
-
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
 
 
 class ProfileCreateActivity : AppCompatActivity() {
@@ -16,7 +19,6 @@ class ProfileCreateActivity : AppCompatActivity() {
         initEvent()
     }
 
-
     override fun onBackPressed() {
         super.onBackPressed()
     }
@@ -24,9 +26,43 @@ class ProfileCreateActivity : AppCompatActivity() {
     fun initEvent(){
         val createProfile = findViewById<Button>(R.id.createProfileBtn)
         createProfile.setOnClickListener{
-            Log.d("errorcode","error_1 detect")
             val intent = Intent(this,MainActivity::class.java)
             startActivity(intent)
         }
+        val findAdressBtn = findViewById<Button>(R.id.findAdressBtn)
+        findAdressBtn.setOnClickListener{
+            val addressInput = findViewById<EditText>(R.id.addressEditText)
+            val geocoder = Geocoder(this);
+            val str: String = addressInput.getText().toString()
+            var list = geocoder.getFromLocationName(str, 10)
+            if (list != null) {
+                val city = ""
+                val country = ""
+                if (list.size == 0) {
+                    //address_result.setText("올바른 주소를 입력해주세요. ")
+                } else {
+                    val address: Address = list.get(0)
+                    val lat: Double = address.getLatitude()
+                    val lon: Double = address.getLongitude()
+                }
+            }
+
+            val dialogTemp = AlertDialog.Builder(this)
+            val dialog = dialogTemp.create()
+            val dialogView = layoutInflater.inflate(R.layout.address_dialog1,null)
+            val findBtn = dialogView.findViewById<Button>(R.id.findBtn)
+            val confirmBtn = dialogView.findViewById<Button>(R.id.confirmButton)
+            dialog.setView(dialogView)
+            dialog.show()
+            findBtn.setOnClickListener{
+
+            }
+            confirmBtn.setOnClickListener{
+                dialog.dismiss()
+            }
+        }
+
+
+
     }
 }
