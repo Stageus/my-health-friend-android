43c2e8d32825dbcdeec1c617cb37fedce8045790
RipAGu <dudgus907@naver.com>
2022-08-02 14:00:48 +0900

    dev

---
+++ app/src/main/java/com/example/myhealthpartner/MainPageActivity.kt
@@ -0,0 +1,30 @@
+package com.example.myhealthpartner
+
+import android.app.Dialog
+import android.media.Image
+import android.os.Bundle
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+
+class MainPageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_page)
+        initEvent()
+    }
+
+
+    fun initEvent(){
+        val matchingFragment = MainPage_MatchingFragment()
+        supportFragmentManager.beginTransaction().replace(R.id.fragmentBox, matchingFragment).commit()
+        val drawerLayout = findViewById<DrawerLayout>(R.id.drawerLayout)
+        val naviBtn = findViewById<ImageButton>(R.id.navBtn)
+        naviBtn.setOnClickListener{
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
+
+
+    }
+}
---
+++ app/src/main/java/com/example/myhealthpartner/MainPage_MatchingFragment.kt
@@ -0,0 +1,66 @@
+package com.example.myhealthpartner
+
+import android.app.AlertDialog
+import android.media.Image
+import android.os.Bundle
+import android.view.ContextThemeWrapper
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.Fragment
+
+class MainPage_MatchingFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.main_page_matching_fragment, container, false)
+        initEvent(view)
+        return view
+    }
+
+    fun initEvent(myView : View){
+        val fitnessListBtn = myView.findViewById<ImageButton>(R.id.fitnessListBtn)
+        fitnessListBtn.setOnClickListener {
+            choiceDialog()
+
+        }
+    }
+
+    fun choiceDialog(){
+        val builder = AlertDialog.Builder(ContextThemeWrapper(context, R.style.AlertDialogCustom))
+        val selectionItem: ArrayList<String> = arrayListOf()
+        builder.setTitle("Test")
+        builder.setMultiChoiceItems(R.array.fitnessList, null){
+            p0, which, isChecked ->
+
+            val fitness : Array<String> = resources.getStringArray(R.array.fitnessList)
+
+            if(isChecked){
+                selectionItem.add(fitness[which])
+            }
+            else{
+                selectionItem.remove(fitness[which])
+            }
+        }
+
+        builder.setPositiveButton("ok"){
+            p0, p1 ->
+            var selections = ArrayList<String>()
+            for(item: String in selectionItem){
+                selections.add(item)
+            }
+        }
+
+        builder.setNegativeButton("cancel"){
+            dialog, p1 -> dialog.cancel()
+        }
+        builder.show()
+    }
+
+
+
+}
