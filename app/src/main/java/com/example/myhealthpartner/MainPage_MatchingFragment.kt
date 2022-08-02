c8610878007bb2511cfc4f9d1fa326113c7ebfa6
RipAGu <102031830+RipAGu@users.noreply.github.com>
2022-08-02 14:09:28 +0900

    Update MainPage_MatchingFragment.kt

--- app/src/main/java/com/example/myhealthpartner/MainPage_MatchingFragment.kt
+++ app/src/main/java/com/example/myhealthpartner/MainPage_MatchingFragment.kt
@@ -1,11 +1,14 @@
 package com.example.myhealthpartner
 
+import android.app.AlertDialog
+import android.media.Image
 import android.os.Bundle
+import android.view.ContextThemeWrapper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageButton
+import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
 
 class MainPage_MatchingFragment : Fragment() {
@@ -18,12 +21,43 @@ class MainPage_MatchingFragment : Fragment() {
         initEvent(view)
         return view
     }
-    fun initEvent(myView: View){
-        val matchingstartBtn = myView.findViewById<Button>(R.id.matchingstartBtn)
-        val changeMainpageFragment = context as ChangeMainpageFragment
 
-        matchingstartBtn.setOnClickListener{
-            changeMainpageFragment.change(2)
+
+    fun initEvent(myView: View) {
+        val fitnessListBtn = myView.findViewById<ImageButton>(R.id.fitnessListBtn)
+        fitnessListBtn.setOnClickListener {
+            choiceDialog()
+
         }
     }
+
+    fun choiceDialog() {
+        val builder = AlertDialog.Builder(ContextThemeWrapper(context, R.style.AlertDialogCustom))
+        val selectionItem: ArrayList<String> = arrayListOf()
+        builder.setTitle("Test")
+        builder.setMultiChoiceItems(R.array.fitnessList, null) { p0, which, isChecked ->
+
+            val fitness: Array<String> = resources.getStringArray(R.array.fitnessList)
+
+            if (isChecked) {
+                selectionItem.add(fitness[which])
+            } else {
+                selectionItem.remove(fitness[which])
+            }
+        }
+
+        builder.setPositiveButton("ok") { p0, p1 ->
+            var selections = ArrayList<String>()
+            for (item: String in selectionItem) {
+                selections.add(item)
+            }
+        }
+
+        builder.setNegativeButton("cancel") { dialog, p1 ->
+            dialog.cancel()
+        }
+        builder.show()
+    }
 }
+
+
