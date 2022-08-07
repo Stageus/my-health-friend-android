1f385b0f3cb440b101db96262363d000c886493b
hyosoek <tennfin1@gmail.com>
2022-08-07 14:24:03 +0900

    Apply GeoCode API

--- app/src/main/java/com/example/myhealthpartner/AccountPage_FindPwFragment.kt
+++ app/src/main/java/com/example/myhealthpartner/AccountPage_FindPwFragment.kt
@@ -4,6 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 
 class AccountPage_FindPwFragment : Fragment() {
@@ -13,6 +17,26 @@ class AccountPage_FindPwFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.account_page_find_pw_fragment, container, false)
+        val pwEdit = view.findViewById<EditText>(R.id.pwEditText)
+        val pwcheckEdit = view.findViewById<EditText>(R.id.pwCheckEditText)
+        pwEdit.visibility = View.GONE
+        pwcheckEdit.visibility = View.GONE
+        initEvent(view)
         return view
+    }
+    fun initEvent(view: View){
+        val changeBtn = view.findViewById<Button>(R.id.re)
+        changeBtn.setOnClickListener{
+            val idEdit = view.findViewById<EditText>(R.id.idEditText)
+            val nameEdit = view.findViewById<EditText>(R.id.nameEditText)
+            //idEdit.setBackgroundResource()
+            idEdit.isEnabled =false
+            nameEdit.isEnabled =false
+
+            val pwEdit = view.findViewById<EditText>(R.id.pwEditText)
+            val pwcheckEdit = view.findViewById<EditText>(R.id.pwCheckEditText)
+            pwEdit.visibility = View.VISIBLE
+            pwcheckEdit.visibility = View.VISIBLE
+        }
     }
 }
