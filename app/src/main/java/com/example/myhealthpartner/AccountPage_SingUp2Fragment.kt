de4ca9f050d2a43deafd1f6044af4c76d17d098d
RipAGu <dudgus907@naver.com>
2022-08-07 23:13:30 +0900

    dev

--- app/src/main/java/com/example/myhealthpartner/AccountPage_SingUp2Fragment.kt
+++ app/src/main/java/com/example/myhealthpartner/AccountPage_SingUp2Fragment.kt
@@ -7,24 +7,22 @@ import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.Fragment
 
-class AccountPage_SignUp1Fragment : Fragment() {
+class AccountPage_SingUp2Fragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.account_page_sign_up1_fragment, container, false)
+        val view = inflater.inflate(R.layout.account_page_sign_up2_fragment, container, false)
         initEvent(view)
         return view
     }
-
 
     fun initEvent(myView : View){
         val changeFragment = context as ChangeFragment
         val nextBtn = myView.findViewById<Button>(R.id.nextBtn)
         nextBtn.setOnClickListener {
-            changeFragment.change(2)
-
+            changeFragment.change(3)
         }
     }
 }
