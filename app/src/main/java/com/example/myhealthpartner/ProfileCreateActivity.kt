5fb7124b5bf683552d5bf843478e076fc85ae3b3
RipAGu <dudgus907@naver.com>
2022-08-12 01:28:29 +0900

    dev

--- app/src/main/java/com/example/myhealthpartner/ProfileCreateActivity.kt
+++ app/src/main/java/com/example/myhealthpartner/ProfileCreateActivity.kt
@@ -2,28 +2,100 @@ package com.example.myhealthpartner
 
 import android.app.AlertDialog
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.location.Address
 import android.location.Geocoder
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.view.View
 import android.widget.*
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import java.io.File
 import java.lang.Exception
 import java.util.*
+import java.util.jar.Manifest
 
 
 class ProfileCreateActivity : AppCompatActivity() {
+
+    lateinit var imageResult : ActivityResultLauncher<Intent>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.create_profile_page)
         val checkboxLinear = findViewById<LinearLayout>(R.id.checkboxLinear)
         checkboxLinear.visibility = View.GONE
+
+        var imageFile : File
+
+        imageResult = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ){
+            result ->
+            if(result.resultCode == RESULT_OK){
+                val imageUri = result.data?.data
+                imageUri.let{
+                    imageFile = File(getRealPathFromURI(it!!))
+                }
+            }
+        }
+
         initEvent()
     }
 
+    companion object{
+        const val REVIEW_MIN_LENGTH = 10
+        const val REQ_GALLERY = 1
+        const val PARAM_KEY_IMAGE = "image"
+        const val PARAM_KEY_PRODUCT_ID = "product_id"
+        const val PARAM_KEY_REVIEW = "review_content"
+        const val PARAM_KEY_RATING = "rating"
+    }
+
+    fun getRealPathFromURI(uri : Uri) : String{
+        val buildName = Build.MANUFACTURER
+        if(buildName.equals("Xiaomi")){
+            return uri.path!!
+        }
+        var columnIndex = 0
+        val proj = arrayOf(MediaStore.Images.Media.DATA)
+        val cursor = contentResolver.query(uri, proj, null, null, null)
+        if(cursor!!.moveToFirst()){
+            columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
+        }
+        val result = cursor.getString(columnIndex)
+        cursor.close()
+        return result
+    }
+
+
     override fun onBackPressed() {
         super.onBackPressed()
+    }
+
+    private fun selectGallery(){
+        val readPermission = ContextCompat.checkSelfPermission(this,
+        android.Manifest.permission.READ_EXTERNAL_STORAGE)
+
+        if(readPermission == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), REQ_GALLERY
+            )
+        }else{
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+            "image/*")
+            imageResult.launch(intent)
+        }
+
+
     }
 
     fun getGeocodeFromAddress(address: String): Address {
@@ -45,11 +117,24 @@ class ProfileCreateActivity : AppCompatActivity() {
 //        return
 //    }
 
+    fun navigateGallery(){
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, 2000)
+
+    }
+
 
 
     fun initEvent(){
         val checkboxLinear = findViewById<LinearLayout>(R.id.checkboxLinear)
         val fitnessListBtn = findViewById<ImageButton>(R.id.fitnessListBtn)
+        val setprofileBtn = findViewById<Button>(R.id.setProfileBtn)
+
+        setprofileBtn.setOnClickListener {
+            selectGallery()
+        }
+
 
         fitnessListBtn.setOnClickListener {
             if(checkboxLinear.visibility == View.GONE){
