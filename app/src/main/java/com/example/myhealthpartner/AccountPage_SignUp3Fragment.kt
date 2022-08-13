81cfd1066f30da5d77a945c334636d808633f91a
RipAGu <dudgus907@naver.com>
2022-08-13 19:52:41 +0900

    dev

--- app/src/main/java/com/example/myhealthpartner/AccountPage_SignUp3Fragment.kt
+++ app/src/main/java/com/example/myhealthpartner/AccountPage_SignUp3Fragment.kt
@@ -5,11 +5,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.ImageButton
-import android.widget.LinearLayout
-import android.widget.TextView
+import android.widget.*
 import androidx.fragment.app.Fragment
+import java.util.regex.Pattern
+
 
 class AccountPage_SignUp3Fragment : Fragment() {
     override fun onCreateView(
@@ -22,32 +21,59 @@ class AccountPage_SignUp3Fragment : Fragment() {
         return view
     }
 
+    fun warningAlert(msg : String){
+        val dialogTemp2 = AlertDialog.Builder(context)
+        val dialog2 = dialogTemp2.create()
+        val dialogViewTemp = layoutInflater.inflate(R.layout.common_alert_dialog,null)
+        val alertMessage = dialogViewTemp.findViewById<TextView>(R.id.alertMessage)
+        alertMessage.text = msg
+        dialog2.setView(dialogViewTemp)
+        dialog2.show()
+        dialogViewTemp.findViewById<Button>(R.id.confirmButton).setOnClickListener{
+            dialog2.dismiss()
+        }
+    }
+
+    fun checkTel(myView: View) : Boolean{
+        val tel = myView.findViewById<EditText>(R.id.phoneNumEditText)
+        if(!Pattern.matches("^01([0|1|6|7|8|9])-?([0-9]{3,4})-?([0-9]{4})$",tel.text.toString())){
+            warningAlert("올바른 전화번호를 입력해주세요!")
+            return false
+        }
+        else return true
+    }
+
+
+
     fun initEvent(myView : View){
+        var telChecked = false
+
         val changeFragment = context as ChangeFragment
         val signupBtn = myView.findViewById<Button>(R.id.signUpBtn)
         val certificationLayout = myView.findViewById<LinearLayout>(R.id.certificationLayout)
+        val getCertificationBtn = myView.findViewById<Button>(R.id.getCertificationBtn)
         certificationLayout.visibility = View.GONE
 
-        signupBtn.setOnClickListener {
-            val dialogTemp2 = AlertDialog.Builder(context)
-            val dialog2 = dialogTemp2.create()
-            val dialogViewTemp = layoutInflater.inflate(R.layout.common_alert_dialog,null)
-            val alertMessage = dialogViewTemp.findViewById<TextView>(R.id.alertMessage)
-            alertMessage.text = "회원가입 성공!"
-            dialog2.setView(dialogViewTemp)
-            dialog2.show()
-            dialogViewTemp.findViewById<Button>(R.id.confirmButton).setOnClickListener{
-                dialog2.dismiss()
-            }
-            changeFragment.change(0)
-        }
-        myView.findViewById<Button>(R.id.getCertificationBtn).setOnClickListener{
-            if(certificationLayout.visibility == View.GONE){
+        getCertificationBtn.setOnClickListener {
+            telChecked = checkTel(myView)
+            if(telChecked == true){
                 certificationLayout.visibility = View.VISIBLE
             }
-            else{
-                certificationLayout.visibility = View.GONE
-            }
+
         }
+
+
+        signupBtn.setOnClickListener {
+            if(telChecked == true){
+                warningAlert("회원가입 성공!")
+                changeFragment.change(0)
+            }
+            else{
+                warningAlert("전화번호인증을 해주세요!")
+            }
+
+        }
+
+
     }
 }
--- app/src/main/java/com/example/myhealthpartner/AccountPage_SingUp2Fragment.kt
+++ app/src/main/java/com/example/myhealthpartner/AccountPage_SingUp2Fragment.kt
@@ -1,10 +1,14 @@
 package com.example.myhealthpartner
 
+import android.app.AlertDialog
 import android.os.Bundle
+import android.util.Patterns
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 
 class AccountPage_SingUp2Fragment : Fragment() {
@@ -18,11 +22,55 @@ class AccountPage_SingUp2Fragment : Fragment() {
         return view
     }
 
+    fun warningAlert(msg : String){
+        val dialogTemp2 = AlertDialog.Builder(context)
+        val dialog2 = dialogTemp2.create()
+        val dialogViewTemp = layoutInflater.inflate(R.layout.common_alert_dialog,null)
+        val alertMessage = dialogViewTemp.findViewById<TextView>(R.id.alertMessage)
+        alertMessage.text = msg
+        dialog2.setView(dialogViewTemp)
+        dialog2.show()
+        dialogViewTemp.findViewById<Button>(R.id.confirmButton).setOnClickListener{
+            dialog2.dismiss()
+        }
+    }
+
+    fun nameCheck(myView: View) : Boolean{
+        val name = myView.findViewById<EditText>(R.id.nameEditText)
+        if(name.text.toString() == ""){
+            return false
+        }
+        else{
+            return true
+        }
+    }
+
+    fun emailCheck(myView: View) : Boolean{
+        val email = myView.findViewById<EditText>(R.id.mailEditText)
+        if(!Patterns.EMAIL_ADDRESS.matcher(email.text.toString()).matches()){
+            return false
+        }
+        else return true
+    }
+
     fun initEvent(myView : View){
         val changeFragment = context as ChangeFragment
         val nextBtn = myView.findViewById<Button>(R.id.nextBtn)
         nextBtn.setOnClickListener {
-            changeFragment.change(3)
+            val nameChecked  = nameCheck(myView)
+            val emailChecked = emailCheck(myView)
+            if(nameChecked == true){
+                if(emailChecked == true){
+                    changeFragment.change(3)
+                }
+                else {
+                    warningAlert("올바른 이메일을 입력해주세요.")
+                }
+            }
+            else{
+                warningAlert("이름을 입력해주세요.")
+            }
+
         }
     }
 
