8ce53e46c754fedff321f3f9753920bddd8be82a
hyosoek <tennfin1@gmail.com>
2022-08-22 20:50:16 +0900

    Update 8.22 RPM

--- app/src/main/java/com/example/myhealthpartner/MyProfilePageActivity.kt
+++ app/src/main/java/com/example/myhealthpartner/MyProfilePageActivity.kt
@@ -1,22 +1,16 @@
 package com.example.myhealthpartner
 
 import android.annotation.SuppressLint
-import android.content.Context
-import android.content.Intent
+import android.content.SharedPreferences
+import android.graphics.Color
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentStateManagerControl
-import androidx.lifecycle.Lifecycle
-import androidx.viewpager.widget.ViewPager
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
+import com.bumptech.glide.Glide
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 
@@ -64,6 +58,61 @@ class MyProfilePageActivity : AppCompatActivity() {
         findViewById<TextView>(R.id.address).text = "주소 - ${loginData.getString("address", "")}"
         findViewById<TextView>(R.id.RPM).text = "${loginData.getInt("rpm", 0)}RPM"
 
+        //실제론 DB가 들어가는 부분
+        if(findViewById<TextView>(R.id.nickname).text == "갓효석"){
+            imageSetTestCode()
+        }
+        changeRPM(loginData)
+
+    }
+
+    fun imageSetTestCode(){
+        val profileImage = findViewById<ImageView>(R.id.profileImage)
+        val scale = resources.displayMetrics.density
+        profileImage.background
+        val padding_5p = (20 * scale + 0.5f).toInt()
+        profileImage.setPadding(0,0,0,0)
+        Glide.with(this) //이미지 적용
+            .load(R.mipmap.ronnie)
+            .circleCrop()
+            .into(profileImage)
+    }
+
+    fun changeRPM(loginData: SharedPreferences) {
+        val tempRPM = loginData.getInt("rpm",0)
+        if(tempRPM < 50) {
+            findViewById<TextView>(R.id.RPM).setTextColor(resources.getColor(R.color.black, null))
+            findViewById<ImageView>(R.id.rpmImage).setImageResource(R.mipmap.blackheart)
+        }
+        else if((tempRPM >= 50) && (tempRPM <70)){
+            findViewById<TextView>(R.id.RPM).setTextColor(resources.getColor(R.color.bronze,null))
+            findViewById<ImageView>(R.id.rpmImage).setImageResource(R.mipmap.bronzeheart)
+        }
+        else if((tempRPM >= 70) && (tempRPM <90)){
+            findViewById<TextView>(R.id.RPM).setTextColor(resources.getColor(R.color.silver,null))
+            findViewById<ImageView>(R.id.rpmImage).setImageResource(R.mipmap.silverheart)
+        }
+        else if((tempRPM >= 90) && (tempRPM <110)){
+            findViewById<TextView>(R.id.RPM).setTextColor(resources.getColor(R.color.gold,null))
+            findViewById<ImageView>(R.id.rpmImage).setImageResource(R.mipmap.goldheart)
+        }
+        else if((tempRPM >= 110) && (tempRPM <130)){
+            findViewById<TextView>(R.id.RPM).setTextColor(resources.getColor(R.color.platinum,null))
+            findViewById<ImageView>(R.id.rpmImage).setImageResource(R.mipmap.platinumheart)
+        }
+        else if((tempRPM >= 130) && (tempRPM <150)){
+            findViewById<TextView>(R.id.RPM).setTextColor(resources.getColor(R.color.diamond,null))
+            findViewById<ImageView>(R.id.rpmImage).setImageResource(R.mipmap.diaheart)
+        }
+        else if(tempRPM >= 150){
+            findViewById<TextView>(R.id.RPM).setTextColor(resources.getColor(R.color.red,null))
+            findViewById<ImageView>(R.id.rpmImage).setImageResource(R.mipmap.redheart)
+        }
+        else{
+            findViewById<TextView>(R.id.RPM).setTextColor(resources.getColor(R.color.black,null))
+            findViewById<ImageView>(R.id.rpmImage).setImageResource(R.mipmap.blackheart)
+        }
+        findViewById<TextView>(R.id.RPM).text = "${loginData.getInt("rpm", 0)}RPM"
 
 
     }
@@ -99,16 +148,7 @@ class MyProfilePageActivity : AppCompatActivity() {
     }
 
     fun initEvent(){
-        val changProfileBtn = findViewById<Button>(R.id.changeProfileBtn)
-//        changProfileBtn.setOnClickListener {
-//            val intent = Intent(applicationContext, ProfileCreate1Activity::class.java)
-//            intent.putExtra("Lat",latTemp)
-//            intent.putExtra("Lng",lngTemp)
-//            startActivity(intent)
-//
-//        }
         checkExercise()
-
         initData()
     }
 }
